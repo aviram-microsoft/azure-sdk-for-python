715de36dc516d7f8ed7937ab7324dfe49bbc039c
reutgross <reutgross@microsoft.com>
2023-01-30 15:22:51 +0200

    new cadl

---
+++ sdk/healthdecisionsupport/azure-ai-healthdecisionsupport/tests/async_onco.py
@@ -0,0 +1,41 @@
+import asyncio
+import sys
+import json
+
+from azure.core.polling import LROPoller, NoPolling, PollingMethod
+from azure.ai.healthdecisionsupport import OncoPhenotypeClient
+from azure.core.credentials import AzureKeyCredential
+
+
+def onco_test() -> LROPoller:
+    endpoint = "https://westeurope.api.cognitive.microsoft.com"
+    key = "95c92b4a837a420c83d37ef2689d7f4a"
+
+    onco_client = OncoPhenotypeClient(endpoint=endpoint,
+                                      credential=AzureKeyCredential(key))
+
+    payload = load_json('onco_test_data.json')
+
+    poller = onco_client.begin_infer_oncology_phenotyping(payload, raw_response_hook=my_callback)
+    response = poller.result()
+
+    return response
+
+
+def load_json(path) -> json:
+    with open(path, 'rt', encoding='utf8') as json_file:
+        res = json.load(json_file)
+        return res
+
+
+def my_callback(resp):
+    print(resp.http_response.content)
+
+
+def main():
+    result = onco_test()
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
